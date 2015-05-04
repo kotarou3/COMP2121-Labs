a861edd2abcb9acb4bda6a3ee8fee63c9d4b5e32
小太 <nospam@xiaotai.org>
2015-05-05 02:29:10 +1000

    Lab 4 Part A

---
+++ lab4/a.c
@@ -0,0 +1,23 @@
+#ifdef ALL_ASSEMBLY
+    #error Including C source when ALL_ASSEMBLY is set
+#endif
+
+#include <avr/io.h>
+
+#include "../keypad.h"
+
+#define LED(reg) reg##C
+
+static void onPress(char input) {
+    if ('0' <= input && input <= '9')
+        LED(PORT) = input - '0';
+}
+
+void setup() {
+    keypadSetup();
+
+    LED(DDR) = 0xff;
+    LED(PORT) = 0xff;
+
+    onKeypadPress(onPress);
+}
