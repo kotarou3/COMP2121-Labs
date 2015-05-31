7c07168bff5b71248bdc9f2982846f2d69026847
小太 <nospam@xiaotai.org>
2015-05-31 19:28:34 +1000

    Project: Add assembly version of magnetron

--- project/magnetron.h
+++ project/magnetron.h
@@ -1,6 +1,8 @@
 #ifndef MAGNETRON_H
 #define MAGNETRON_H
 
+#ifndef __ASSEMBLER__
+
 typedef enum _PowerSetting {
     POWER_MAX,
     POWER_HALF,
@@ -10,5 +12,14 @@ typedef enum _PowerSetting {
 
 void magnetronSetup();
 void magnetronSetPower(PowerSetting power);
+
+#else
+
+#define POWER_MAX 0
+#define POWER_HALF (POWER_MAX + 1)
+#define POWER_QUARTER (POWER_HALF + 1)
+#define POWER_OFF (POWER_QUARTER + 1)
+
+#endif
 
 #endif
